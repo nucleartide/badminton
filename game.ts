0caa5b7af0b4f98f4e55e60d459448db4e5cf3bd
Jason Tu <jasontu4@gmail.com>
2018-08-12 21:55:05 -0700

    Add test color to game.ts

--- game.ts
+++ game.ts
@@ -1,3 +1,35 @@
-function _update() {}
+declare function cls(c: col): void
 
-function _draw() {}
+/**
+ * game loop.
+ */
+
+function _update() {
+}
+
+function _draw() {
+  cls(col.indigo)
+}
+
+/**
+ * utils.
+ */
+
+enum col {
+  black,
+  dark_blue,
+  dark_purple,
+  dark_green,
+  brown,
+  dark_gray,
+  light_gray,
+  white,
+  red,
+  orange,
+  yellow,
+  green,
+  blue,
+  indigo,
+  pink,
+  peach,
+}
